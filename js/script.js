e9d153f4d10670462007559a6250c31923a6a899
DiamanDD <66356974+DiamanDD@users.noreply.github.com>
2021-07-08 14:47:10 +0300

    переделка в функцию

--- js/script.js
+++ js/script.js
@@ -1,24 +1,58 @@
 "use strict";
-function upp() {
-    // считываем поле ввода
-   let data=document.querySelector(".pole") .value;
+// считываем поле ввода
+
+function readInput(){
+    let data=document.querySelector(".todo_input") .value;
+    upp(data)
+}
+
+
+function upp(data) {
+
    // если поле не пустое
     if(data!="") {
         // создаем элемент списка
-        let upp = document.createElement("li")
-        // добавляем ему класс
-        upp.className = "lili"
+       let elemLi=creatLi()
         // добавлем кнопку для закрытия
-        let chk = document.createElement("div")
-        chk.className = "x"
+       let btnClose = createClose()
         // добавляем в элемент текст из поля
-        upp.innerHTML = data;
-        // добавляем в конец кнопку закрытия
-        upp.append(chk)
+       elemLi= addText(elemLi,data)
+         // добавляем в конец кнопку закрытия
+        elemLi=addBtnClose(elemLi,btnClose)
+
         // Добавляем в тег Ul наш элемент
-        document.querySelector(".spis").append(upp)
+        addElemHtml(elemLi)
+
 
     }
+}
+function creatLi() {
+    let elemLi = document.createElement("li")
+    elemLi.className = "lili"
+    console.log("вызов1")
+    return elemLi
+}
+
+function createClose() {
+    let btnClose = document.createElement("div")
+    btnClose.className = "x"
+    console.log("вызов2")
+    return btnClose
+}
+
+function addText(elemLi,data) {
+    elemLi.innerHTML = data;
+    return elemLi
+
+}
+
+function addBtnClose(elemLi,btnClose) {
+    elemLi.append(btnClose)
+    return(elemLi)
+}
+
+function addElemHtml(elemLi) {
+    document.querySelector(".todo_elements").append(elemLi)
 }
 
 // зачеркивание
@@ -38,6 +72,6 @@ function del() {
 }
 this.onclick=del
 
-document.querySelector(".upps").addEventListener("click",upp)
+
 
 
